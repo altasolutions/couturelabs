4ddebb66c78a81aed3a82017ab3af06ec51534f7
altasolutions <165282920+altasolutions@users.noreply.github.com>
2024-08-02 23:51:48 +0200

    Update clevertap_v2.js

--- clevertap_v2.js
+++ clevertap_v2.js
@@ -1,8 +1,8 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
+  typeof define === 'function' && define.amd ? define(['clevertap'],factory) :
   (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.clevertap = factory());
-}(this, ('clevertap', function () { 'use strict';
+}(this, (function () { 'use strict';
 
   var id = 0;
 
